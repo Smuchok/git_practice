df898fb47cbef2990cb1aac54ce80a808d9cd993
Smuchok <73115018+Smuchok@users.noreply.github.com>
2023-04-16 13:19:45 +0300

    add read file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 print('Hello world!')
 
 number = 0
+with open('data.txt', 'r') as f:
+  number = int(f.read())
+
 while True:
   inp = input("МЕНЮ:\n1 - Задати число\n2 - Змінити\n3 - Занулити\n0 - Вийти")
   if inp == '1':
